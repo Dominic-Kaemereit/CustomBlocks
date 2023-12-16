23e348dd64d732be5af488dcecafb1f9ae983b25
Dominic <d151l@d151l.de>
2023-12-16 16:02:39 +0100

    update note block protection

--- src/main/java/de/d151l/custom/block/listener/NoteBlockProtectionListener.java
+++ src/main/java/de/d151l/custom/block/listener/NoteBlockProtectionListener.java
@@ -1,11 +1,15 @@
 package de.d151l.custom.block.listener;
 
 import de.d151l.custom.block.CustomBlocks;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class NoteBlockProtectionListener implements Listener {
 
@@ -16,28 +20,52 @@ public class NoteBlockProtectionListener implements Listener {
     }
 
     @EventHandler
-    public void onPlace(BlockPhysicsEvent event) {
+    public void onBlockPhysics(BlockPhysicsEvent event) {
         final Block block = event.getBlock();
+        final Block topBlock = event.getBlock().getRelative(BlockFace.UP);
+        final Block downBlock = event.getBlock().getRelative(BlockFace.DOWN);
+        final Block northBlock = event.getBlock().getRelative(BlockFace.NORTH);
+        final Block eastBlock = event.getBlock().getRelative(BlockFace.EAST);
+        final Block southBlock = event.getBlock().getRelative(BlockFace.SOUTH);
+        final Block westBlock = event.getBlock().getRelative(BlockFace.WEST);
 
-        if (block.getType() != org.bukkit.Material.NOTE_BLOCK)
-            return;
-
-        if (this.plugin.getCustomBlockHandler().isNotCustomBlock(block))
-            return;
-
-        event.setCancelled(true);
+        if (this.checkBlock(block) || this.checkBlock(topBlock) || this.checkBlock(downBlock) || this.checkBlock(northBlock) || this.checkBlock(eastBlock) || this.checkBlock(southBlock) || this.checkBlock(westBlock))
+            event.setCancelled(true);
     }
 
     @EventHandler
     public void onBlockRedstone(BlockRedstoneEvent event) {
         final Block block = event.getBlock();
 
-        if (block.getType() != org.bukkit.Material.NOTE_BLOCK)
+        if (block.getType() != Material.NOTE_BLOCK)
             return;
 
         if (this.plugin.getCustomBlockHandler().isNotCustomBlock(block))
             return;
 
         event.setNewCurrent(event.getOldCurrent());
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        final Player player = event.getPlayer();
+
+        if (event.getClickedBlock() == null)
+            return;
+
+        if (event.getAction().isLeftClick())
+            return;
+
+        if (event.getClickedBlock().getType() != Material.NOTE_BLOCK)
+            return;
+
+        event.setCancelled(true);
+    }
+
+    private boolean checkBlock(Block block) {
+        if (block.getType() != Material.NOTE_BLOCK)
+            return false;
+
+        return !this.plugin.getCustomBlockHandler().isNotCustomBlock(block);
     }
 }
